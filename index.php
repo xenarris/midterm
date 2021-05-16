4bb4bd9433310c0fd800a04017e007d5eafd209f
Jean-Kenneth Antonio <jantonio2@mail.greenriver.edu>
2021-05-16 19:04:18 -0400

    Defined survey route and the survey itself

--- index.php
+++ index.php
@@ -20,5 +20,18 @@ $f3 -> route('GET /', function() {
 }
 );
 
+//Define survey route
+$f3 -> route('GET|POST /survey', function($f3) {
+    //initialize variables
+    $options = array("easy"=>"This Midterm is easy", "likable"=>"I like midterms", "monday"=>"Today is Monday");
+    //set survey options
+    $f3->set('survey', $options);
+
+    //display the homepage
+    $view = new Template();
+    echo $view->render('views/survey.html');
+}
+);
+
 //Run fat free
 $f3->run();
